cb6d38e2475a09a9f786463117806a9af4d3a093
John Robin <johnrobinlugtu@gmail.com>
2018-01-24 23:53:09 -0800

    added more dependencies redux logger and thunk

--- frontend/public/app.jsx
+++ frontend/public/app.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './component/root';
 
+/* TESTING ONLY */
 import * as SessionAPI from './util/session_api_util';
 import * as MangaAPI from './util/manga_api_util';
 
 document.addEventListener('DOMContentLoaded', ()=>{
 
     /* TESTING */
-    window.SessionAPI = SessionAPI;
-    window.MangaAPI = MangaAPI;
+
     /*         */
+    
     const root = document.querySelector('#root');
     ReactDOM.render(<Root />, root);
 });
