50258b5ed36fc95c799871beac900d846f27ec96
John Robin <johnrobinlugtu@gmail.com>
2018-01-26 01:17:39 -0800

    changed background

--- frontend/public/component/navbar/icon.jsx
+++ frontend/public/component/navbar/icon.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 const Icon = ({ image_name }) => {
     return (
         <div className="icon-container">
-            
             <img className="icon" src={"./static/svg/" + image_name + ".svg"} />
         </div>
     );
