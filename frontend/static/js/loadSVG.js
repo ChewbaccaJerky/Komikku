cdef41151868b868c3cbb29387772a5ffdad09c6
John Robin <johnrobinlugtu@gmail.com>
2018-01-26 01:00:51 -0800

    Added loadSVG method

---
+++ frontend/static/js/loadSVG.js
@@ -0,0 +1,19 @@
+function loadSVG(selector, url) {
+    let target = document.querySelector(selector);
+
+    // If SVG is supported
+    if (typeof SVGRect != "undefined") {
+        // Request the SVG file
+        let ajax = new XMLHttpRequest();
+        ajax.open("GET", url + ".svg", true);
+        ajax.send();
+
+        ajax.onload = function(e) {
+            target.innerHTML = ajax.responseText;
+        };
+    }
+    else {
+        // fallback to png
+        target.innerHTML = "<img src='" + url + ".svg' />";
+    }
+}
