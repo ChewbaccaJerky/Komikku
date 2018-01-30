5374af842004cf2782660ae93f3886fd94a843f5
John Robin <johnrobinlugtu@gmail.com>
2018-01-30 15:31:35 -0800

    Added manga action

---
+++ frontend/public/action/manga_action.js
@@ -0,0 +1,40 @@
+import * as MangaAPIUtil from '../util/manga_api_util';
+
+export const RECEIVE_MANGAS = "RECEIVE_MANGAS";
+export const RECEIVE_MANGA = "RECEIVE_MANGA";
+export const RECEIVE_MANGA_ERRORS = "RECEIVE_MANGA_ERRORS";
+
+const receiveMangas = mangas => ({
+    type: RECEIVE_MANGAS,
+    mangas: mangas.mangas,
+    page: mangas.page
+});
+
+const receiveManga = manga => ({
+    type: RECEIVE_MANGA,
+    manga: manga
+});
+
+const receiveErrors = errors => ({
+    type: RECEIVE_MANGA_ERRORS,
+    errors
+});
+
+export const fetchAllMangas = () => dispatch => (
+    MangaAPIUtil.fetchAllMangas().then(payload => (
+        dispatch(receiveMangas(payload))
+        ), err => dispatch(receiveErrors(err)))
+);
+
+export const fetchMangasByPage = (pageNum) => dispatch => (
+    MangaAPIUtil.fetchMangasByPage(pageNum).then(payload => (
+        dispatch(receiveMangas(payload))
+    ), err => dispatch(receiveErrors(err)))
+);
+
+export const fetchManga = (id) => dispatch => (
+    MangaAPIUtil.fetchManga(id).then(manga => (
+        dispatch(receiveManga(manga))
+    ), err => dispatch(receiveErrors(err)))
+);
+
